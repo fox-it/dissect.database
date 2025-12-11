52990405dce8e421ad6a29fb0d8699139d025943
Schamper <1254028+Schamper@users.noreply.github.com>
2025-12-11 22:47:25 +0100

    Refactor security descriptors

--- tests/ese/ntds/test_sd.py
+++ tests/ese/ntds/test_sd.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from dissect.database.ese.ntds.object import Computer
-from dissect.database.ese.ntds.sd import ACCESS_ALLOWED_ACE, AccessMaskFlag, AceFlag
+from dissect.database.ese.ntds.sd import ACCESS_MASK, ACE_FLAGS
 
 if TYPE_CHECKING:
     from dissect.database.ese.ntds.ntds import NTDS
@@ -14,21 +13,21 @@ def test_dacl_specific_user(ntds_small: NTDS) -> None:
     computers = list(ntds_small.computers())
     # Get one sample computer
     esm = next(c for c in computers if c.name == "ESMWVIR1000000")
-    assert isinstance(esm, Computer)
+    # And one sample user
+    user = next(u for u in ntds_small.users() if u.name == "RACHELLE_LYNN")
 
     # Checked using Active Directory User and Computers (ADUC) GUI for user RACHELLE_LYNN
-    ace = next(ace for ace in esm.dacl.aces if next(ntds_small.lookup(objectSid=str(ace.sid))).name == "RACHELLE_LYNN")
-    assert isinstance(ace, ACCESS_ALLOWED_ACE)
-    assert ace.has_flag(AceFlag.CONTAINER_INHERIT_ACE)
-    assert ace.has_flag(AceFlag.INHERITED_ACE)
+    ace = next(ace for ace in esm.dacl.ace if ace.sid == user.sid)
+    assert ACE_FLAGS.CONTAINER_INHERIT_ACE in ace.flags
+    assert ACE_FLAGS.INHERITED_ACE in ace.flags
 
-    assert ace.mask.has_priv(AccessMaskFlag.WRITE_OWNER)
-    assert ace.mask.has_priv(AccessMaskFlag.WRITE_DACL)
-    assert ace.mask.has_priv(AccessMaskFlag.READ_CONTROL)
-    assert ace.mask.has_priv(AccessMaskFlag.DELETE)
-    assert ace.mask.has_priv(AccessMaskFlag.ADS_RIGHT_DS_CONTROL_ACCESS)
-    assert ace.mask.has_priv(AccessMaskFlag.ADS_RIGHT_DS_CREATE_CHILD)
-    assert ace.mask.has_priv(AccessMaskFlag.ADS_RIGHT_DS_DELETE_CHILD)
-    assert ace.mask.has_priv(AccessMaskFlag.ADS_RIGHT_DS_READ_PROP)
-    assert ace.mask.has_priv(AccessMaskFlag.ADS_RIGHT_DS_WRITE_PROP)
-    assert ace.mask.has_priv(AccessMaskFlag.ADS_RIGHT_DS_SELF)
+    assert ACCESS_MASK.WRITE_OWNER in ace.mask
+    assert ACCESS_MASK.WRITE_DACL in ace.mask
+    assert ACCESS_MASK.READ_CONTROL in ace.mask
+    assert ACCESS_MASK.DELETE in ace.mask
+    assert ACCESS_MASK.ADS_RIGHT_DS_CONTROL_ACCESS in ace.mask
+    assert ACCESS_MASK.ADS_RIGHT_DS_CREATE_CHILD in ace.mask
+    assert ACCESS_MASK.ADS_RIGHT_DS_DELETE_CHILD in ace.mask
+    assert ACCESS_MASK.ADS_RIGHT_DS_READ_PROP in ace.mask
+    assert ACCESS_MASK.ADS_RIGHT_DS_WRITE_PROP in ace.mask
+    assert ACCESS_MASK.ADS_RIGHT_DS_SELF in ace.mask
