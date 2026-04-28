da20b8d9938c930efa1d4dc79b2cdd9bc7d10a7f
twiggler <12800443+twiggler@users.noreply.github.com>
2026-04-28 14:30:48 +0000

    fix: qualify reference

--- tests/_docs/conf.py
+++ tests/_docs/conf.py
@@ -26,7 +26,7 @@ autoapi_options = [
     "undoc-members",
     "show-inheritance",
     "show-module-summary",
-    "special-members"
+    "special-members",
 ]
 autoapi_keep_files = True
 autoapi_template_dir = "_templates/autoapi"
@@ -38,5 +38,5 @@ autosectionlabel_prefix_document = True
 
 suppress_warnings = [
     # https://github.com/readthedocs/sphinx-autoapi/issues/285
-    "autoapi.python_import_resolution"
+    "autoapi.python_import_resolution",
 ]
